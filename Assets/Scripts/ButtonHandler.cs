3d8868302b3e553eb457f74d9f6f9b3e5108fbd3
Flying Katsu <Warped2713@users.noreply.github.com>
2016-09-09 19:47:10 -0400

    adjusted SelectDevice to fit all resolutions; Added SwitchScene and Quit button functions; initial TitleMenu buttons and text

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+
+	public void SwitchSceneByName (string name) {
+		SceneManager.LoadScene (name, LoadSceneMode.Single);
+	}
+
+	public void CloseApplication() {
+		Application.Quit ();
+	}
+}
