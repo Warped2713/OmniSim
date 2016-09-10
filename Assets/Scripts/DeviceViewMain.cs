42cb8115cd69257e4f547e5b4301a05dded21051
Flying Katsu <Warped2713@users.noreply.github.com>
2016-09-10 02:07:31 -0400

    DeviceView now loads current device at start

---
+++ Assets/Scripts/DeviceViewMain.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceViewMain : MonoBehaviour {
+
+	public float distanceToCamera;
+
+	private Device device;
+	private Vector3 initDevicePos;
+
+	// Use this for initialization
+	void Start () {
+
+		// Load in the current device
+		this.device = GlobalVars.Instance.getCurrentDevice();
+		string prefabFilepath = "Prefabs/Devices/" + this.device.PrefabFilename;
+		this.device.Model = Instantiate (Resources.Load(prefabFilepath, typeof(GameObject))) as GameObject;
+
+		// Set the position of model to distanceToCamera from camera position and centered on XY plane
+		Vector3 campos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+		this.device.Model.transform.position = this.initDevicePos = new Vector3( campos.x, device.Model.transform.localScale.y / 2 - campos.y, campos.z + this.distanceToCamera );
+
+		// Populate the Presets Tab
+
+		// Populate the Aliens Tab
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Handle camera zoom for when panels are all collapsed/expanded
+	}
+}
