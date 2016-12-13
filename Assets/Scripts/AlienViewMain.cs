1c6a5303f536bd2c4e6c598e2f5f9367c3d40d9b
Flying Katsu <Warped2713@users.noreply.github.com>
2016-12-13 06:36:00 -0500

    cleaned up references, updated fonts in UI layouts

--- Assets/Scripts/AlienViewMain.cs
+++ Assets/Scripts/AlienViewMain.cs
@@ -8,6 +8,8 @@ public class AlienViewMain : MonoBehaviour {
 	public float positionY;
 	public float positionZ;
 
+	public Vector3 initRotation;
+
 	private Alien alien;
 
 	// Use this for initialization
@@ -23,7 +25,8 @@ public class AlienViewMain : MonoBehaviour {
 		// Set the position of model 
 		//Vector3 campos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
 		//this.device.Model.transform.position = this.initDevicePos = new Vector3( this.positionX, this.positionY, this.positionZ );
-
+		this.alien.Model.transform.position = new Vector3( this.alien.Model.transform.position.x + this.positionX, this.alien.Model.transform.position.y + this.positionY, this.alien.Model.transform.position.z + this.positionZ );
+		this.alien.Model.transform.Rotate(initRotation);
 
 		// Initialize Text Fields
 		Text alienName = GameObject.Find("AlienName").GetComponent<Text>();
