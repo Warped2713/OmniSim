bb50015bd633e9f483df536a8278979e43d689d0
Flying Katsu <Warped2713@users.noreply.github.com>
2016-12-20 03:37:38 -0500

    created scene for recording autospin models

--- Assets/Scripts/ModelFX/AutoSpin.cs
+++ Assets/Scripts/ModelFX/AutoSpin.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rotation : MonoBehaviour {
+public class AutoSpin : MonoBehaviour {
+
+	public float degPerFrame = 5.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate (0, degPerFrame, 0);
 	}
 }
