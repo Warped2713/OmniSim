62e6eff71aa132e566c3cc5826722415c565f0ef
Flying Katsu <Warped2713@users.noreply.github.com>
2016-12-24 04:14:03 -0500

    added an auto zoom for green room

---
+++ Assets/Scripts/Camera/ZoomToPosition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomToPosition : MonoBehaviour {
+
+	public Vector3 targetPosition = Vector3.zero;
+	public float minDistance = 5f;
+	public float stepDistance = 2f;
+
+	float distanceRemaining = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		distanceRemaining = Vector3.Distance(transform.position, targetPosition);
+
+		if ( distanceRemaining > minDistance + stepDistance ) {
+			Vector3.MoveTowards (transform.position, targetPosition, stepDistance);
+		}
+
+	}
+}
