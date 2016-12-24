4f25770e896c9020f42b8e2523bcef97d79cef5c
Flying Katsu <Warped2713@users.noreply.github.com>
2016-12-24 04:38:04 -0500

    fixed greymatter scale z, corrected camera zoom behavior and added easing

--- Assets/Scripts/Camera/ZoomToPosition.cs
+++ Assets/Scripts/Camera/ZoomToPosition.cs
@@ -5,13 +5,16 @@ public class ZoomToPosition : MonoBehaviour {
 
 	public Vector3 targetPosition = Vector3.zero;
 	public float minDistance = 5f;
-	public float stepDistance = 2f;
+	public float initialStepDistance = 2f;
 
 	float distanceRemaining = 0f;
+	float stepDistance;
+	float initialDistance;
 
 	// Use this for initialization
 	void Start () {
-
+		stepDistance = initialStepDistance;
+		initialDistance = Vector3.Distance(transform.position, targetPosition);
 	}
 	
 	// Update is called once per frame
@@ -19,8 +22,12 @@ public class ZoomToPosition : MonoBehaviour {
 
 		distanceRemaining = Vector3.Distance(transform.position, targetPosition);
 
-		if ( distanceRemaining > minDistance + stepDistance ) {
-			Vector3.MoveTowards (transform.position, targetPosition, stepDistance);
+		if (distanceRemaining > minDistance + initialStepDistance * 5f) {
+			stepDistance = initialStepDistance * (distanceRemaining / initialDistance);
+		}
+
+		if ( distanceRemaining > minDistance ) {
+			transform.position = Vector3.MoveTowards (transform.position, targetPosition, stepDistance);
 		}
 
 	}
